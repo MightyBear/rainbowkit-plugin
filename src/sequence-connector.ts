5c67229b64593ce5aaf1254c662e30dac57a6adb
paragmightybear <parag@mightybeargames.com>
2023-03-09 11:35:29 +0800

    checking for window before initialising sequence wallet

--- src/sequence-connector.ts
+++ src/sequence-connector.ts
@@ -1,115 +1,119 @@
-import { sequence } from '0xsequence';
-import { mainnetNetworks, testnetNetworks } from '@0xsequence/network';
-import type { ConnectOptions, Web3Provider } from '@0xsequence/provider';
-import { Wallet } from '@0xsequence/provider';
-import { Chain } from '@rainbow-me/rainbowkit';
-import { Connector, ConnectorData, ConnectorNotFoundError, UserRejectedRequestError } from 'wagmi';
+import { sequence } from "0xsequence";
+import { mainnetNetworks, testnetNetworks } from "@0xsequence/network";
+import type { ConnectOptions, Web3Provider } from "@0xsequence/provider";
+import { Wallet } from "@0xsequence/provider";
+import { Chain } from "@rainbow-me/rainbowkit";
+import { Connector, ConnectorData, ConnectorNotFoundError, UserRejectedRequestError } from "wagmi";
 
 interface Options {
-  connect?: ConnectOptions;
+	connect?: ConnectOptions;
 }
 
 export class SequenceConnector extends Connector<Web3Provider, Options | undefined> {
-  id = 'sequence';
-  name = 'Sequence';
-  // chains = chainConfigList
-  ready = true;
-  provider: Web3Provider | null = null;
-  wallet: Wallet;
-  connected = false;
-  constructor({ chains, options }: { chains?: Chain[]; options?: Options }) {
-    super({ chains, options });
-    sequence.initWallet();
-    this.wallet = sequence.getWallet();
-  }
-  async connect(): Promise<Required<ConnectorData<Web3Provider>>> {
-    if (!this.wallet.isConnected()) {
-      // @ts-ignore-next-line
-      this?.emit('message', { type: 'connecting' })
-      const e = await this.wallet.connect(this.options?.connect);
-      if (e.error) {
-        throw new UserRejectedRequestError(e.error);
-      }
-      if (!e.connected) {
-        throw new UserRejectedRequestError('Wallet connection rejected');
-      }
-    }
+	id = "sequence";
+	name = "Sequence";
+	// chains = chainConfigList
+	ready = true;
+	provider: Web3Provider | null = null;
+	wallet: Wallet;
+	connected = false;
+	constructor({ chains, options }: { chains?: Chain[]; options?: Options }) {
+		super({ chains, options });
+		sequence.initWallet();
+		this.wallet = sequence.getWallet();
+	}
+	async connect(): Promise<Required<ConnectorData<Web3Provider>>> {
+		if (!this.wallet.isConnected()) {
+			// @ts-ignore-next-line
+			this?.emit("message", { type: "connecting" });
+			const e = await this.wallet.connect(this.options?.connect);
+			if (e.error) {
+				throw new UserRejectedRequestError(e.error);
+			}
+			if (!e.connected) {
+				throw new UserRejectedRequestError("Wallet connection rejected");
+			}
+		}
 
-    const chainId = await this.getChainId();
-    const provider = await this.getProvider();
-    const account = await this.getAccount();
-    provider.on("accountsChanged", this.onAccountsChanged);
-    this.wallet.on('chainChanged', this.onChainChanged);
-    provider.on('disconnect', this.onDisconnect);
-    this.connected = true;
-    return {
-      account,
-      chain: {
-        id: chainId,
-        unsupported: this.isChainUnsupported(chainId),
-      },
-      provider,
-    };
-  }
-  async disconnect() {
-    this.wallet.disconnect();
-  }
-  getAccount() {
-    return this.wallet.getAddress() as Promise<`0x${string}`>;
-  }
-  getChainId() {
-    // in mobile, when connecting with sequence Rainbowkit first tried to get ChainID for some reason, but in sequence you can't get ChainID before being connected, so forcing here to connect if you want to get ChainID
-    if (!this.wallet.isConnected()) {
-      return this.connect().then(() => this.wallet.getChainId());
-    }
-    return this.wallet.getChainId();
-  }
-  async getProvider() {
-    if (!this.provider) {
-      const provider = this.wallet.getProvider();
-      if (!provider) {
-        throw new ConnectorNotFoundError('Failed to get Sequence Wallet provider.');
-      }
-      this.provider = provider;
-    }
-    return this.provider;
-  }
-  async getSigner() {
-    return this.wallet.getSigner();
-  }
-  async isAuthorized() {
-    try {
-      const account = await this.getAccount();
-      return !!account;
-    } catch {
-      return false;
-    }
-  }
-  async switchChain(chainId: number): Promise<Chain> {
-    await this.provider?.send('wallet_switchEthereumChain', [{ chainId }]);
-    return { id: chainId } as Chain;
-  }
-  protected onAccountsChanged = (accounts: string[]) => {
-    return { account: accounts[0] };
-  };
-  protected onChainChanged = (chain: number | string) => {
-    this.provider?.emit('chainChanged', chain);
-    const id = normalizeChainId(chain);
-    const unsupported = this.isChainUnsupported(id);
-    // @ts-ignore-next-line
-    this?.emit('change', { chain: { id, unsupported } })
-  };
-  protected onDisconnect = () => {
-    // @ts-ignore-next-line
-    this?.emit('disconnect')
-  };
-  isChainUnsupported(chainId: number): boolean {
-    return !(mainnetNetworks.some((c) => c.chainId === chainId) || testnetNetworks.some((c) => c.chainId === chainId));
-  }
+		const chainId = await this.getChainId();
+		const provider = await this.getProvider();
+		const account = await this.getAccount();
+		provider.on("accountsChanged", this.onAccountsChanged);
+		this.wallet.on("chainChanged", this.onChainChanged);
+		provider.on("disconnect", this.onDisconnect);
+		this.connected = true;
+		return {
+			account,
+			chain: {
+				id: chainId,
+				unsupported: this.isChainUnsupported(chainId),
+			},
+			provider,
+		};
+	}
+	async disconnect() {
+		this.wallet.disconnect();
+	}
+	getAccount() {
+		return this.wallet.getAddress() as Promise<`0x${string}`>;
+	}
+	getChainId() {
+		// in mobile, when connecting with sequence Rainbowkit first tried to get ChainID for some reason, but in sequence you can't get ChainID before being connected, so forcing here to connect if you want to get ChainID
+		if (!this.wallet.isConnected()) {
+			return this.connect().then(() => this.wallet.getChainId());
+		}
+		return this.wallet.getChainId();
+	}
+	async getProvider() {
+		if (!this.provider) {
+			const provider = this.wallet.getProvider();
+			if (!provider) {
+				throw new ConnectorNotFoundError("Failed to get Sequence Wallet provider.");
+			}
+			this.provider = provider;
+		}
+		return this.provider;
+	}
+	async getSigner() {
+		return this.wallet.getSigner();
+	}
+	async isAuthorized() {
+		try {
+			const account = await this.getAccount();
+			return !!account;
+		} catch {
+			return false;
+		}
+	}
+	async switchChain(chainId: number): Promise<Chain> {
+		await this.provider?.send("wallet_switchEthereumChain", [{ chainId }]);
+		return { id: chainId } as Chain;
+	}
+	protected onAccountsChanged = (accounts: string[]) => {
+		return { account: accounts[0] };
+	};
+	protected onChainChanged = (chain: number | string) => {
+		this.provider?.emit("chainChanged", chain);
+		const id = normalizeChainId(chain);
+		const unsupported = this.isChainUnsupported(id);
+		// @ts-ignore-next-line
+		this?.emit("change", { chain: { id, unsupported } });
+	};
+	protected onDisconnect = () => {
+		// @ts-ignore-next-line
+		this?.emit("disconnect");
+	};
+	isChainUnsupported(chainId: number): boolean {
+		return !(
+			mainnetNetworks.some(c => c.chainId === chainId) ||
+			testnetNetworks.some(c => c.chainId === chainId)
+		);
+	}
 }
 
 function normalizeChainId(chainId: string | number | bigint) {
-  if (typeof chainId === 'string') return Number.parseInt(chainId, chainId.trim().substring(0, 2) === '0x' ? 16 : 10);
-  if (typeof chainId === 'bigint') return Number(chainId);
-  return chainId;
+	if (typeof chainId === "string")
+		return Number.parseInt(chainId, chainId.trim().substring(0, 2) === "0x" ? 16 : 10);
+	if (typeof chainId === "bigint") return Number(chainId);
+	return chainId;
 }
--- src/sequence-wallet.ts
+++ src/sequence-wallet.ts
@@ -1,52 +1,74 @@
-import type { ConnectOptions } from '@0xsequence/provider';
-import { Chain, Wallet } from '@rainbow-me/rainbowkit';
+import type { ConnectOptions } from "@0xsequence/provider";
+import { Chain, getWalletConnectConnector, Wallet } from "@rainbow-me/rainbowkit";
 
-import { SequenceConnector } from './sequence-connector';
+import { SequenceConnector } from "./sequence-connector";
 
 export interface MyWalletOptions {
-  chains: Chain[];
-  connect?: ConnectOptions
+	chains: Chain[];
+	connect?: ConnectOptions;
+}
+function isRainbow(ethereum: NonNullable<typeof window["ethereum"]>) {
+	// `isRainbow` needs to be added to the wagmi `Ethereum` object
+	const isRainbow = Boolean(ethereum.isRainbow);
+
+	if (!isRainbow) {
+		return false;
+	}
+
+	return true;
 }
 
-export const sequenceWallet = ({ chains, connect }: MyWalletOptions): Wallet => ({
-  id: 'sequence',
-  name: 'Sequence',
-  iconUrl: 'https://user-images.githubusercontent.com/26363061/210380889-f338d084-c42a-477a-ad1e-dd776658ba3f.svg',
-  iconBackground: '#fff',
-  downloadUrls: {
-    browserExtension: 'https://sequence.app',
-  },
-  createConnector: () => {
-    const connector = new SequenceConnector({
-      chains,
-      options: {
-        connect
-      },
-    });
+export const sequenceWallet = ({ chains, connect }: MyWalletOptions): Wallet => {
+	const isRainbowInjected =
+		typeof window !== "undefined" &&
+		typeof window.ethereum !== "undefined" &&
+		isRainbow(window.ethereum);
 
-    return {
-      connector,
-      mobile: {
-        getUri: async () => {
-          try {
-            await connector.connect();
-            return window.location.href;
-          } catch (e) {
-            console.error('Failed to connect');
-          }
-          return '';
-        },
-      },
-      desktop: {
-        getUri: async () => {
-          try {
-            await connector.connect();
-          } catch (e) {
-            console.error('Failed to connect');
-          }
-          return '';
-        },
-      },
-    };
-  },
-});
+	const shouldUseWalletConnect = !isRainbowInjected;
+
+	return {
+		id: "sequence",
+		name: "Sequence",
+		iconUrl:
+			"https://user-images.githubusercontent.com/26363061/210380889-f338d084-c42a-477a-ad1e-dd776658ba3f.svg",
+		iconBackground: "#fff",
+		downloadUrls: {
+			browserExtension: "https://sequence.app",
+		},
+		createConnector: () => {
+			const connector = shouldUseWalletConnect
+				? getWalletConnectConnector({ chains })
+				: new SequenceConnector({
+						chains,
+						options: {
+							connect,
+						},
+				  });
+
+			return {
+				connector,
+				mobile: {
+					getUri: async () => {
+						try {
+							await connector.connect();
+							return window.location.href;
+						} catch (e) {
+							console.error("Failed to connect");
+						}
+						return "";
+					},
+				},
+				desktop: {
+					getUri: async () => {
+						try {
+							await connector.connect();
+						} catch (e) {
+							console.error("Failed to connect");
+						}
+						return "";
+					},
+				},
+			};
+		},
+	};
+};
